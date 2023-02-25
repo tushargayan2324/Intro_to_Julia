5d76443d78ba62f97ae857cc73df3711b4fa8cb6
tushargayan2324 <55867762+tushargayan2324@users.noreply.github.com>
2023-02-25 11:38:34 +0530

    Add files via upload

--- Solutions/mandelbrot_heatmap.jl
+++ Solutions/mandelbrot_heatmap.jl
@@ -1,23 +1,9 @@
 using Plots
 
-"""
-function iterated_value(z::Complex)
-    c = z
-    iterations = 50
-    for n = 1:iterations
-        if abs2(z) > 4
-            return n-1
-    
-        end
-        z = z^2 + c
-    end
-    return iterations
-end"""
-
 function iterated_value(x,y)
     #c = z
     p,q = x,y
-    iterations = 100
+    iterations = 125
     for n = 1:iterations
         if (x^2 + y^2) > 4
             return n-1
@@ -28,38 +14,24 @@ function iterated_value(x,y)
     return iterations
 end
 
-
-
-function mandel_brot(x_range,y_range)
-    #iterated_value(0.4,0.01 )
-
-    #A = zeros(Float64, (1000, 750))
-
-    
+function mandelbrot(x_range,y_range)
     n = size(x_range)[1]
     m = size(y_range)[1]
 
-    #A = fill(complex(0.0,0.0), (n,m))
+    A = zeros(Int, (m,n))
 
-    A = zeros(Int, (n,m))
-
-    #A[1,1] = iterated_value(0.1,0.1)
-    #A
-
-    #typeof(A[1,1])
-
-    for j in 1:m, i in 1:n
-        #println(j/m)
-        A[i, j] = iterated_value(x_range[i],y_range[j])
+    for i in 1:n, j in 1:m
+        A[j, i] = iterated_value(x_range[i],y_range[j])
     end
 
     return A
 end
 
-#A
-x_range = -1.5:0.0004:0.5
-y_range = -1:0.0004:1
 
+x_range = -1.5:0.1:0.5
+y_range = -1:0.1:1
 
-heatmap(mandel_brot(x_range,y_range); size=(2000,2500))
+#show(IOContext(stdout, :limit=>false), MIME"text/plain"(), mandel_brot(x_range,y_range))
+
+heatmap(mandelbrot(x_range,y_range); size=(1200,950), legend=false)
 savefig("mandelbrot_plot.png")
